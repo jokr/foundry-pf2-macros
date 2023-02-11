19aa2449968ffd4eaa43dabe385170907637d6d7
Joel Krebs <joel.krebs@gmail.com>
2023-02-11 17:16:15 +0000

    Update scouting.js
    expand scouting macro to sync with scout

--- scouting.js
+++ scouting.js
@@ -1,16 +1,45 @@
-async function applyEffect(actors, effect) {
+async function createSource(effect) {
   const source = (await fromUuid(effect)).toObject();
   source.flags = mergeObject(source.flags ?? {}, {
     core: { sourceId: effect },
   });
+  return source;
+}
+
+function hasEffect(actor, effect) {
+  return actor.itemTypes.effect.find(
+    (e) => e.flags.core?.sourceId === effect
+  );
+}
+
+async function toggleEffect(actors, effect) {
+  const source = await createSource(effect);
   for (const actor of actors) {
-    const existing = actor.itemTypes.effect.find(
-      (e) => e.flags.core?.sourceId === effect
-    );
+    const existing = hasEffect(actor, effect);
     if (existing) {
       await existing.delete();
     } else {
       await actor.createEmbeddedDocuments("Item", [source]);
+    }
+  }
+}
+
+async function addEffect(actors, effect) {
+  const source = await createSource(effect);
+  for (const actor of actors) {
+    const existing = hasEffect(actor, effect);
+    if (!existing) {
+      await actor.createEmbeddedDocuments("Item", [source]);
+    }
+  }
+}
+
+async function removeEffect(actors, effect) {
+  const source = await createSource(effect);
+  for (const actor of actors) {
+    const existing = hasEffect(actor, effect);
+    if (existing) {
+      await existing.delete();
     }
   }
 }
@@ -36,14 +65,19 @@ async function runScouting() {
    * and then replace the ID here.
    */
   const SCOUT_UUID = "Item.eyUmSXjpO5WPRW7c"; // Scout
-  await applyEffect(actors, SCOUT_UUID);
+  await toggleEffect(actors, SCOUT_UUID);
+  const isScouting = actors.find(a => hasEffect(a, SCOUT_UUID));
 
   const allPlayers = canvas.tokens.placeables
     .filter((t) => t.actor.isOfType("character"))
     .flatMap((t) => t.actor);
 
   const SCOUTING_UUID = "Compendium.pf2e.other-effects.EMqGwUi3VMhCjTlF"; // Scouting
-  await applyEffect(allPlayers, SCOUTING_UUID);
+  if (isScouting) {
+    await addEffect(allPlayers, SCOUTING_UUID);
+  } else {
+    await removeEffect(allPlayers, SCOUTING_UUID);
+  }
 }
 
 await runScouting();
